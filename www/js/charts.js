37ea8d14df19566e85ca39f8acd2355accfc18a5
antonyesudas <antonyesudas@gmail.com>
2016-01-16 20:03:59 +0530

    sidemenu opened, graph labels

--- www/js/charts.js
+++ www/js/charts.js
@@ -6,24 +6,40 @@ $(document).ready(function(){
 	var s1 = [2, 6, 4, 9, 6, 10];
 	var ticks = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 	
-	plot1 = $.jqplot('chartdiv', [s1], {
+	plotChart = $.jqplot('chartdiv', [s1], {
 		// Only animate if we're not using excanvas (not in IE 7 or IE 8)..
 		animate: !$.jqplot.use_excanvas,
 		seriesDefaults:{
 			renderer:$.jqplot.BarRenderer,
 			pointLabels: { show: true }
 		},
+		axesDefaults: {
+			tickOptions: {
+				fontFamily: 'Verdana',
+				fontSize: '10pt'
+			}
+		},		
+		series:[{showMarker:false}],
 		axes: {
 			xaxis: {
-				label:'Months',
+				label:'MONTHS',
 				renderer: $.jqplot.CategoryAxisRenderer,
 				ticks: ticks
 			},
 			yaxis:{
-			  label:'Dollars',
-			  labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+				label:'DOLLARS',
+				labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+				tickOptions: {
+				  formatString: '$%d'
+				}
 			}
-		},
-		highlighter: { show: false }
+		}
+		
+	
 	});
+	
+	window.onresize = function(event) {
+		plotChart.replot();
+	}
+	
 });
